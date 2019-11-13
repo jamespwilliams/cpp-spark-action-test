0db0f07c98a6e542a58f28b7b4bf7f9bb6fb5e02
James Williams <jamesphwilliams@gmail.com>
2019-11-13 20:26:47 +0000

    Simplify project structure

--- src/integer.hpp
+++ src/integer.hpp
@@ -2,13 +2,5 @@ namespace integers {
     int add(int a, int b);
 
     int multiply(int a, int b);
-
-    int divide_round_down(int a, int b);
-
-    int divide_round_up(int a, int b);
-
-    double divide(int a, int b);
-
 }
-
 
--- testcases/test_integer.cpp
+++ testcases/test_integer.cpp
@@ -16,23 +16,3 @@ SPARK_TEST_CASE("test_multiply","Test that the integer multiplication function w
     REQUIRE(multiply(-7,-9) == -7 * -9);
     REQUIRE(multiply(10000,1) == 10000 * 1);
 }
-
-SPARK_TEST_CASE("test_divide_round_down","Test that the integer division function rounds down.", "divide", "Divides two integers."){
-    //todo consider testing behavior on division by zero
-    REQUIRE(divide_round_down(7,-9) == -1);
-    REQUIRE(divide_round_down(11,3) == (11 / 3));
-    REQUIRE(divide_round_down(-11,3) == -4);
-}
-
-SPARK_TEST_CASE("test_divide_round_up","Test that the integer division function rounds up.", "divide", "Divides two integers."){
-    //todo consider testing behavior on division by zero
-    REQUIRE(divide_round_up(7,-9) == 0);
-    REQUIRE(divide_round_up(11,3) == (11 / 3) + 1);
-    REQUIRE(divide_round_up(-11,3) == -3);
-}
-
-SPARK_TEST_CASE("test_divide","Test that the integer division function works.", "divide", "Divides two integers."){
-    REQUIRE(divide(7,-9) == Approx(((double)7) / ((double)-9)));
-    REQUIRE(divide(11,3) == Approx(((double)11 / ((double)3))));
-    REQUIRE(divide(-11,3) == Approx(((double)-11 / ((double)3))));
-}
